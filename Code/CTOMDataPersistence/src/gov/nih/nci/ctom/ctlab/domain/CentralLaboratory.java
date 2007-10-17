102c73672eb5c247f75986f512dc222569692b77
Unknown <unknown@unknown.com>
2007-10-17 21:48:38 +0000

    Added to abstract DAO out to a common module
    SVN-Revision: 26

---
+++ Code/CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/CentralLaboratory.java
@@ -0,0 +1,46 @@
+package gov.nih.nci.ctom.ctlab.domain;
+
+public class CentralLaboratory {
+
+	
+	private String identifier = null;
+	private String name = null;
+	private Long orgId;
+	/**
+	 * @return the identifier
+	 */
+	public String getIdentifier() {
+		return identifier;
+	}
+	/**
+	 * @param identifier the identifier to set
+	 */
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the orgId
+	 */
+	public Long getOrgId() {
+		return orgId;
+	}
+	/**
+	 * @param orgId the orgId to set
+	 */
+	public void setOrgId(Long orgId) {
+		this.orgId = orgId;
+	}
+
+}
