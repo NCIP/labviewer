0440133983119f110cc341a1e135bb3481d50080
Unknown <unknown@unknown.com>
2007-10-25 14:30:34 +0000

    changed the domain classes according to the updated data model
    SVN-Revision: 35

--- Code/CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/CentralLaboratory.java
+++ Code/CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/CentralLaboratory.java
@@ -1,11 +1,18 @@
 package gov.nih.nci.ctom.ctlab.domain;
 
+import java.util.Date;
+
 public class CentralLaboratory {
 
 	
 	private String identifier = null;
 	private String name = null;
-	private Long orgId;
+	private Long id;
+	private String source;
+	private Date srcExtractDt;
+	private Date ctomInsertDt;
+	private Date ctomUpdateDt;
+	
 	/**
 	 * @return the identifier
 	 */
@@ -31,16 +38,64 @@ public class CentralLaboratory {
 		this.name = name;
 	}
 	/**
-	 * @return the orgId
+	 * @return the Id
 	 */
-	public Long getOrgId() {
-		return orgId;
+	public Long getId() {
+		return id;
 	}
 	/**
-	 * @param orgId the orgId to set
+	 * @param Id the Id to set
 	 */
-	public void setOrgId(Long orgId) {
-		this.orgId = orgId;
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the source
+	 */
+	public String getSource() {
+		return source;
+	}
+	/**
+	 * @param source the source to set
+	 */
+	public void setSource(String source) {
+		this.source = source;
+	}
+	/**
+	 * @return the srcExtractDt
+	 */
+	public Date getSrcExtractDt() {
+		return srcExtractDt;
+	}
+	/**
+	 * @param srcExtractDt the srcExtractDt to set
+	 */
+	public void setSrcExtractDt(Date srcExtractDt) {
+		this.srcExtractDt = srcExtractDt;
+	}
+	/**
+	 * @return the ctomInsertDt
+	 */
+	public Date getCtomInsertDt() {
+		return ctomInsertDt;
+	}
+	/**
+	 * @param ctomInsertDt the ctomInsertDt to set
+	 */
+	public void setCtomInsertDt(Date ctomInsertDt) {
+		this.ctomInsertDt = ctomInsertDt;
+	}
+	/**
+	 * @return the ctomUpdateDt
+	 */
+	public Date getCtomUpdateDt() {
+		return ctomUpdateDt;
+	}
+	/**
+	 * @param ctomUpdateDt the ctomUpdateDt to set
+	 */
+	public void setCtomUpdateDt(Date ctomUpdateDt) {
+		this.ctomUpdateDt = ctomUpdateDt;
 	}
 
 }
