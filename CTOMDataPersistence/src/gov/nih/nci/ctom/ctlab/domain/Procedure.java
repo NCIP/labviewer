1b17fb4d714deb5c3ce254773dcadd3c8b7902ec
Unknown <unknown@unknown.com>
2007-10-30 14:05:07 +0000

    changed the domain classes according to the updated data model
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@45 a393f10d-60de-4571-9f1a-620cac27c168

--- CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/Procedure.java
+++ CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/Procedure.java
@@ -1,13 +1,22 @@
 package gov.nih.nci.ctom.ctlab.domain;
 
+import java.util.Date;
+
 /**
  * @author griffinch
  *
  */
 public class Procedure {
 	private Long id;
+	private String anatomicSiteCd;
+	private String anatomicSiteCdSys;
 	private String fastingStatus;
-	private SpecimenCollection specimenCollection;
+	private String subtypeCd;
+	private String source;
+	private Date srcExtractDt;
+	private Date ctomInsertDt;
+	private Date ctomUpdateDt;
+	private SpecimenCollection specimenCollection=null;
 
 	/**
 	 * @return the id
@@ -49,5 +58,103 @@ public class Procedure {
 	 */
 	public void setFastingStatus(String fastingStatus) {
 		this.fastingStatus = fastingStatus;
+	}
+
+	/**
+	 * @return the anatomicSiteCd
+	 */
+	public String getAnatomicSiteCd() {
+		return anatomicSiteCd;
+	}
+
+	/**
+	 * @param anatomicSiteCd the anatomicSiteCd to set
+	 */
+	public void setAnatomicSiteCd(String anatomicSiteCd) {
+		this.anatomicSiteCd = anatomicSiteCd;
+	}
+
+	/**
+	 * @return the anatomicSiteCdSys
+	 */
+	public String getAnatomicSiteCdSys() {
+		return anatomicSiteCdSys;
+	}
+
+	/**
+	 * @param anatomicSiteCdSys the anatomicSiteCdSys to set
+	 */
+	public void setAnatomicSiteCdSys(String anatomicSiteCdSys) {
+		this.anatomicSiteCdSys = anatomicSiteCdSys;
+	}
+
+	/**
+	 * @return the subtypeCd
+	 */
+	public String getSubtypeCd() {
+		return subtypeCd;
+	}
+
+	/**
+	 * @param subtypeCd the subtypeCd to set
+	 */
+	public void setSubtypeCd(String subtypeCd) {
+		this.subtypeCd = subtypeCd;
+	}
+
+	/**
+	 * @return the source
+	 */
+	public String getSource() {
+		return source;
+	}
+
+	/**
+	 * @param source the source to set
+	 */
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	/**
+	 * @return the srcExtractDt
+	 */
+	public Date getSrcExtractDt() {
+		return srcExtractDt;
+	}
+
+	/**
+	 * @param srcExtractDt the srcExtractDt to set
+	 */
+	public void setSrcExtractDt(Date srcExtractDt) {
+		this.srcExtractDt = srcExtractDt;
+	}
+
+	/**
+	 * @return the ctomInsertDt
+	 */
+	public Date getCtomInsertDt() {
+		return ctomInsertDt;
+	}
+
+	/**
+	 * @param ctomInsertDt the ctomInsertDt to set
+	 */
+	public void setCtomInsertDt(Date ctomInsertDt) {
+		this.ctomInsertDt = ctomInsertDt;
+	}
+
+	/**
+	 * @return the ctomUpdateDt
+	 */
+	public Date getCtomUpdateDt() {
+		return ctomUpdateDt;
+	}
+
+	/**
+	 * @param ctomUpdateDt the ctomUpdateDt to set
+	 */
+	public void setCtomUpdateDt(Date ctomUpdateDt) {
+		this.ctomUpdateDt = ctomUpdateDt;
 	}
 }
--- CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/Specimen.java
+++ CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/domain/Specimen.java
@@ -1,17 +1,27 @@
 package gov.nih.nci.ctom.ctlab.domain;
 
+import java.util.Date;
+
 public class Specimen {
 
-	private String accessionNumber = null;
+	private Long id;
+	private Long sampleIDentifier;
 	private String sampleIdentifierOrig = null;
+	private Long volume;
+	private String volumeOrg;
+	private String volumeUOMCd;
+	private String accessionNumber = null;
+	private String condition = null;
 	private String commentsFromLaboratory = null;
 	private String commentsFromInvestigator = null;
-	private String condition = null;
-	private String sampleTypeCd = null;
-	private String sampleTypeCdSystem = null;
-	private String sampleTypeCdSystemName = null;
-	private Long procedureActivityId;
-	private Long id;
+	private Long sampleTypeCdId;
+	private Long specimenCollectionId;
+	private int securityKey;
+	private String source;
+	private Date srcExtractDt;
+	private Date ctomInsertDt;
+	private Date ctomUpdateDt;
+	
 	/**
 	 * @return the accessionNumber
 	 */
@@ -74,28 +84,28 @@ public class Specimen {
 	}
 	/**
 	 * @return the sampleTypeCd
-	 */
+	 *//*
 	public String getSampleTypeCd() {
 		return sampleTypeCd;
 	}
-	/**
+	*//**
 	 * @param sampleTypeCd the sampleTypeCd to set
-	 */
+	 *//*
 	public void setSampleTypeCd(String sampleTypeCd) {
 		this.sampleTypeCd = sampleTypeCd;
 	}
-	/**
+	*//**
 	 * @return the procedureActivityId
-	 */
+	 *//*
 	public Long getProcedureActivityId() {
 		return procedureActivityId;
 	}
-	/**
+	*//**
 	 * @param procedureActivityId the procedureActivityId to set
-	 */
+	 *//*
 	public void setProcedureActivityId(Long procedureActivityId) {
 		this.procedureActivityId = procedureActivityId;
-	}
+	}*/
 	/**
 	 * @return the id
 	 */
@@ -110,27 +120,159 @@ public class Specimen {
 	}
 	/**
 	 * @return the sampleTypeCdSystem
-	 */
+	 *//*
 	public String getSampleTypeCdSystem() {
 		return sampleTypeCdSystem;
 	}
-	/**
+	*//**
 	 * @param sampleTypeCdSystem the sampleTypeCdSystem to set
-	 */
+	 *//*
 	public void setSampleTypeCdSystem(String sampleTypeCdSystem) {
 		this.sampleTypeCdSystem = sampleTypeCdSystem;
 	}
-	/**
+	*//**
 	 * @return the sampleTypeCdSystemName
-	 */
+	 *//*
 	public String getSampleTypeCdSystemName() {
 		return sampleTypeCdSystemName;
 	}
-	/**
+	*//**
 	 * @param sampleTypeCdSystemName the sampleTypeCdSystemName to set
-	 */
+	 *//*
 	public void setSampleTypeCdSystemName(String sampleTypeCdSystemName) {
 		this.sampleTypeCdSystemName = sampleTypeCdSystemName;
+	}*/
+	/**
+	 * @return the sampleIDentifier
+	 */
+	public Long getSampleIDentifier() {
+		return sampleIDentifier;
+	}
+	/**
+	 * @param sampleIDentifier the sampleIDentifier to set
+	 */
+	public void setSampleIDentifier(Long sampleIDentifier) {
+		this.sampleIDentifier = sampleIDentifier;
+	}
+	/**
+	 * @return the volume
+	 */
+	public Long getVolume() {
+		return volume;
+	}
+	/**
+	 * @param volume the volume to set
+	 */
+	public void setVolume(Long volume) {
+		this.volume = volume;
+	}
+	/**
+	 * @return the volumeOrg
+	 */
+	public String getVolumeOrg() {
+		return volumeOrg;
+	}
+	/**
+	 * @param volumeOrg the volumeOrg to set
+	 */
+	public void setVolumeOrg(String volumeOrg) {
+		this.volumeOrg = volumeOrg;
+	}
+	/**
+	 * @return the volumeUOMCd
+	 */
+	public String getVolumeUOMCd() {
+		return volumeUOMCd;
+	}
+	/**
+	 * @param volumeUOMCd the volumeUOMCd to set
+	 */
+	public void setVolumeUOMCd(String volumeUOMCd) {
+		this.volumeUOMCd = volumeUOMCd;
+	}
+	/**
+	 * @return the sampleTypeCdId
+	 */
+	public Long getSampleTypeCdId() {
+		return sampleTypeCdId;
+	}
+	/**
+	 * @param sampleTypeCdId the sampleTypeCdId to set
+	 */
+	public void setSampleTypeCdId(Long sampleTypeCdId) {
+		this.sampleTypeCdId = sampleTypeCdId;
+	}
+	/**
+	 * @return the specimenCollectionId
+	 */
+	public Long getSpecimenCollectionId() {
+		return specimenCollectionId;
+	}
+	/**
+	 * @param specimenCollectionId the specimenCollectionId to set
+	 */
+	public void setSpecimenCollectionId(Long specimenCollectionId) {
+		this.specimenCollectionId = specimenCollectionId;
+	}
+	/**
+	 * @return the securityKey
+	 */
+	public int getSecurityKey() {
+		return securityKey;
+	}
+	/**
+	 * @param securityKey the securityKey to set
+	 */
+	public void setSecurityKey(int securityKey) {
+		this.securityKey = securityKey;
+	}
+	/**
+	 * @return the source
+	 */
+	public String getSource() {
+		return source;
+	}
+	/**
+	 * @param source the source to set
+	 */
+	public void setSource(String source) {
+		this.source = source;
+	}
+	/**
+	 * @return the srcExtractDt
+	 */
+	public Date getSrcExtractDt() {
+		return srcExtractDt;
+	}
+	/**
+	 * @param srcExtractDt the srcExtractDt to set
+	 */
+	public void setSrcExtractDt(Date srcExtractDt) {
+		this.srcExtractDt = srcExtractDt;
+	}
+	/**
+	 * @return the ctomInsertDt
+	 */
+	public Date getCtomInsertDt() {
+		return ctomInsertDt;
+	}
+	/**
+	 * @param ctomInsertDt the ctomInsertDt to set
+	 */
+	public void setCtomInsertDt(Date ctomInsertDt) {
+		this.ctomInsertDt = ctomInsertDt;
+	}
+	/**
+	 * @return the ctomUpdateDt
+	 */
+	public Date getCtomUpdateDt() {
+		return ctomUpdateDt;
+	}
+	/**
+	 * @param ctomUpdateDt the ctomUpdateDt to set
+	 */
+	public void setCtomUpdateDt(Date ctomUpdateDt) {
+		this.ctomUpdateDt = ctomUpdateDt;
 	}
 	
 
