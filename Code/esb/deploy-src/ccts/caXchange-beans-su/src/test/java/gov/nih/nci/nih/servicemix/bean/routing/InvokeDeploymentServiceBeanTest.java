1f8b87f854fa120bf2fd73f10807f6e4fa524e59
Unknown <unknown@unknown.com>
2007-11-16 23:27:06 +0000

    added for testing
    SVN-Revision: 127

---
+++ Code/esb/deploy-src/ccts/caXchange-beans-su/src/test/java/gov/nih/nci/nih/servicemix/bean/routing/InvokeDeploymentServiceBeanTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package gov.nih.nci.nih.servicemix.bean.routing;
+
+import java.io.InputStream;
+
+import javax.jbi.messaging.ExchangeStatus;
+import javax.jbi.messaging.InOut;
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.servicemix.client.DefaultServiceMixClient;
+import org.apache.servicemix.tck.SpringTestSupport;
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+
+/**
+ * @author griffinch
+ *
+ */
+public class InvokeDeploymentServiceBeanTest extends SpringTestSupport {
+
+    public void testSubmitValidCredentialsMessage() throws Exception {
+    	DefaultServiceMixClient client = new DefaultServiceMixClient(jbi);
+    	InputStream fis = getClass().getResourceAsStream("testmessage.xml");
+            InOut me = client.createInOutExchange();
+            me.getInMessage().setProperty("Test", "TestProperty");
+            me.getInMessage().setContent(new StreamSource(fis));
+            me.setService(new QName("http://servicemix.apache.org/caXchange/setSubjectService", "setSubjectEndPoint"));
+            me.setOperation(new QName("http://servicemix.apache.org/caXchange/setSubjectService", "setSubjectEndPoint"));
+            client.sendSync(me);
+            System.out.println(me);
+            assertEquals(ExchangeStatus.ACTIVE, me.getStatus());
+            assertTrue(me.getMessage("out") != null);
+            assertEquals("TestProperty", me.getMessage("out").getProperty("Test"));
+        }
+
+    /* public void testSubmitInValidCredentialsMessage() throws Exception {
+    	DefaultServiceMixClient client = new DefaultServiceMixClient(jbi);
+    	InputStream fis = getClass().getResourceAsStream("EnrollPatient.xml");
+            InOut me = client.createInOutExchange();
+            me.getInMessage().setProperty("Test", "TestProperty");
+            me.getInMessage().setContent(new StreamSource(fis));
+            me.setService(new QName("http://servicemix.apache.org/caXchange/c3d", "enrollPatient"));
+            me.setOperation(new QName("http://servicemix.apache.org/caXchange/c3d", "enrollPatient"));
+            client.sendSync(me);
+            System.out.println(me);
+            assertEquals(ExchangeStatus.ACTIVE, me.getStatus());
+            assertTrue(me.getMessage("out") != null);
+            assertEquals("TestProperty", me.getMessage("out").getProperty("Test"));
+        }
+
+    */
+	
+	
+	/* (non-Javadoc)
+	 * @see org.apache.servicemix.tck.SpringTestSupport#createBeanFactory()
+	 */
+	@Override
+	protected AbstractXmlApplicationContext createBeanFactory() {
+		// TODO Auto-generated method stub
+		return new ClassPathXmlApplicationContext("spring.xml");
+	}
+
+}
