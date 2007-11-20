73e2c52f1699be9f57c6ae83c73f107504daa4c4
Unknown <unknown@unknown.com>
2007-11-20 15:23:59 +0000

    Added to make it a pure grid service project
    SVN-Revision: 146

---
+++ StudyConsumerService/src/gov/nih/nci/ccts/grid/service/globus/resource/StudyConsumerResource.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.ccts.grid.service.globus.resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** 
+ * The implementation of this StudyConsumerResource type.
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class StudyConsumerResource extends BaseResourceBase {
+
+
+
+}
