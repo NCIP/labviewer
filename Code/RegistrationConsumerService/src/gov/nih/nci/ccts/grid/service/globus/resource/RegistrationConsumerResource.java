f812b90d45edd8c5a2d643c642f416c4c69000bc
Unknown <unknown@unknown.com>
2007-11-20 14:54:02 +0000

    Added to make it a pure grid service project
    SVN-Revision: 143

---
+++ Code/RegistrationConsumerService/src/gov/nih/nci/ccts/grid/service/globus/resource/RegistrationConsumerResource.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.ccts.grid.service.globus.resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** 
+ * The implementation of this RegistrationConsumerResource type.
+ * 
+ * @created by Introduce Toolkit version 1.1
+ * 
+ */
+public class RegistrationConsumerResource extends BaseResourceBase {
+
+
+
+}
