7b5441438d8fdfc297bd752d92908681f27e39ae
Unknown <unknown@unknown.com>
2007-11-24 17:09:25 +0000

    *** empty log message ***
    SVN-Revision: 162

--- Code/esb/deploy-src/ccts/caXchange-inbound-jms-su/src/test/java/gov/nih/nci/caxchange/jms/inbound/InboundJmsTests.java
+++ Code/esb/deploy-src/ccts/caXchange-inbound-jms-su/src/test/java/gov/nih/nci/caxchange/jms/inbound/InboundJmsTests.java
@@ -1,5 +1,15 @@
 package gov.nih.nci.caxchange.jms.inbound;
 
+/*import javax.activation.DataSource;
+import javax.jbi.messaging.InOut;
+import org.apache.servicemix.client.DefaultServiceMixClient;
+import org.apache.servicemix.jbi.jaxp.StringSource;
+import javax.xml.namespace.QName;
+import javax.jms.*;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+*/
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 
@@ -28,16 +38,40 @@ public class InboundJmsTests extends TestCase {
 		 *  
 		 */
 		
-		try {
+		 
+
 		
-		
+		/*try {
+			ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+			Destination inQueue = new ActiveMQQueue("queueY");
+            Destination outQueue = new ActiveMQQueue("queue/A");
+            Connection connection = factory.createConnection();
+            Session session = connection.createSession(false,Session.CLIENT_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(inQueue);
+            MessageConsumer consumer = session.createConsumer(outQueue);
+            connection.start();
+            
+
+            System.out.println("Waiting for request form JSR and HTTP SA.");
+             producer.send(session.createTextMessage("<message>Hellow,Yogaraj!</message>"));
+            for (int i = 0 ;i < 800; i++) {
+                    TextMessage tMsg = session
+                                    .createTextMessage("<message>Message #"+i+" Hello, Yogaraj!</message>");
+                    System.out.println(tMsg.getText());
+                    //tMsg.setJMSReplyTo(outQueue);
+                    producer.send(tMsg);
+            }*/
+           
 			
-			
+		try{
 		this.assertTrue(true);
 		} catch (Exception ex) {
+			 ex.printStackTrace();
+			
 			assertTrue(false);
 		}
-	}
+	
+}
 
 	@Override
 	public TestResult run() {
