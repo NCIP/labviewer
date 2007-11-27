cebc00c645d0bd7a780f7fa9ab282994208fe218
Unknown <unknown@unknown.com>
2007-11-27 19:58:03 +0000

    checking if the page was accessed via hot link - if yes then get the patient id and study id for the given studyGridId and forward the page to hot_link success page
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@184 a393f10d-60de-4571-9f1a-620cac27c168

--- ctomlabviewer/src/gov/nih/nci/caxchange/ctom/viewer/actions/LoginAction.java
+++ ctomlabviewer/src/gov/nih/nci/caxchange/ctom/viewer/actions/LoginAction.java
@@ -6,31 +6,29 @@
  */
 package gov.nih.nci.caxchange.ctom.viewer.actions;
 
-import gov.nih.nci.cabig.ctms.web.sso.Utils;
-import gov.nih.nci.cagrid.authentication.bean.BasicAuthenticationCredential;
-import gov.nih.nci.cagrid.authentication.stubs.types.AuthenticationProviderFault;
-import gov.nih.nci.cagrid.authentication.stubs.types.InsufficientAttributeFault;
-import gov.nih.nci.cagrid.authentication.stubs.types.InvalidCredentialFault;
 import gov.nih.nci.caxchange.ctom.viewer.constants.DisplayConstants;
 import gov.nih.nci.caxchange.ctom.viewer.constants.ForwardConstants;
+import gov.nih.nci.caxchange.ctom.viewer.forms.LabActivitiesSearchForm;
 import gov.nih.nci.caxchange.ctom.viewer.forms.LoginForm;
 import gov.nih.nci.caxchange.ctom.viewer.util.LabViewerAuthorizationHelper;
-import gov.nih.nci.caxchange.ctom.viewer.util.ObjectFactory;
-import gov.nih.nci.ctms.interoperability.GridProxyTransformer;
-import gov.nih.nci.logging.api.user.UserInfoHelper;
+import gov.nih.nci.labhub.domain.II;
+import gov.nih.nci.labhub.domain.SubjectAssignment;
 import gov.nih.nci.security.AuthenticationManager;
-import gov.nih.nci.security.AuthorizationManager;
 import gov.nih.nci.security.SecurityServiceProvider;
-import gov.nih.nci.security.UserProvisioningManager;
-import gov.nih.nci.security.authorization.domainobjects.Group;
-import gov.nih.nci.security.authorization.domainobjects.ProtectionElement;
-import gov.nih.nci.security.authorization.domainobjects.ProtectionElementPrivilegeContext;
-import gov.nih.nci.security.authorization.domainobjects.User;
 import gov.nih.nci.security.exceptions.CSConfigurationException;
 import gov.nih.nci.security.exceptions.CSException;
+import gov.nih.nci.system.applicationservice.ApplicationService;
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+import gov.nih.nci.system.query.cql.CQLAssociation;
+import gov.nih.nci.system.query.cql.CQLAttribute;
+import gov.nih.nci.system.query.cql.CQLGroup;
+import gov.nih.nci.system.query.cql.CQLLogicalOperator;
+import gov.nih.nci.system.query.cql.CQLObject;
+import gov.nih.nci.system.query.cql.CQLPredicate;
+import gov.nih.nci.system.query.cql.CQLQuery;
 
-import java.io.File;
-import java.util.Set;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -43,12 +41,6 @@ import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
-import org.globus.gsi.GlobusCredential;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
 
 /**
  * LoginAction handles the authentication for Lab Viewer
@@ -117,6 +109,22 @@ public class LoginAction extends Action
 			HttpSession session = request.getSession(true);
 			session.setAttribute(DisplayConstants.LOGIN_OBJECT,form);
 			session.setAttribute(DisplayConstants.CURRENT_TABLE_ID,DisplayConstants.HOME_ID);
+			if(session.getAttribute("HOT_LINK")=="true")
+				 
+			{  
+				LabActivitiesSearchForm labFm=(LabActivitiesSearchForm)session.getAttribute("CURRENT_FORM");
+				try {
+					
+					getPatientIdRoot((String)session.getAttribute("studySubjectGridId"),labFm);
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				session.setAttribute("CURRENT_FORM", labFm);
+			  	String operation= "execute";
+				return (mapping.findForward(ForwardConstants.LOGIN_SUCCESS_HOTLINK));
+			}	
 			
 			forward = mapping.findForward(ForwardConstants.LOGIN_SUCCESS);
 		}
@@ -163,5 +171,74 @@ public class LoginAction extends Action
 		
 		return forward;
 	}
+	/**
+	 * 
+	 * Select i.extension from identifier i where i.protocol_id in (
+	 * select ss.protocol_id from study_site ss, study_participant_assignment spa, identifier ii where ((ii.root='50113a1a-4c3e-4418-8c37-7383cba1aaf8' and ii.study_participant_assignmnt_id=spa.id 
+	 * and spa.study_site_id=ss.id))) UNION
+	 * select i.extension from identifier i where i.root='50113a1a-4c3e-4418-8c37-7383cba1aaf8' and i.study_participant_assignmnt_id is not null
+	 * @param studyGridId
+	 * @param labFm
+	 * @throws Exception
+	 */
+	private void getPatientIdRoot(String studyGridId,LabActivitiesSearchForm labFm) throws Exception
+	{
 		
+		try
+		{
+			ApplicationService appService = ApplicationServiceProvider.getApplicationService();
+			
+			// Create the query to get SubjectAssignment object
+			CQLQuery query = new CQLQuery();
+			CQLObject target = new CQLObject();
+			target.setName("gov.nih.nci.labhub.domain.SubjectAssignment");
+			//target.setAttribute(new CQLAttribute("studySubjectIdentifier",
+					//CQLPredicate.IS_NOT_NULL,""));
+			
+			// Set the subject Identifier on the association to II
+			CQLAssociation subjectAssignmentAssociation1 = new CQLAssociation();
+			subjectAssignmentAssociation1.setName("gov.nih.nci.labhub.domain.II");
+			subjectAssignmentAssociation1.setTargetRoleName("studySubjectIdentifier");
+			subjectAssignmentAssociation1.setAttribute(new CQLAttribute("root",
+					CQLPredicate.EQUAL_TO, studyGridId.trim()));
+			
+			// Now get to StudySite
+			CQLAssociation subjectAssignmentAssociation2 = new CQLAssociation();
+			subjectAssignmentAssociation2.setName("gov.nih.nci.labhub.domain.StudySite");
+			subjectAssignmentAssociation2.setTargetRoleName("studySite");
+
+			// Now get to Study
+			CQLAssociation studySiteAssociation1 = new CQLAssociation();
+			studySiteAssociation1.setName("gov.nih.nci.labhub.domain.Study");
+			studySiteAssociation1.setTargetRoleName("study");
+			subjectAssignmentAssociation2.setAssociation(studySiteAssociation1);
+			
+			
+			CQLGroup finalgroup = new CQLGroup();
+			finalgroup.addAssociation(subjectAssignmentAssociation1);
+			finalgroup.addAssociation(subjectAssignmentAssociation2);
+			finalgroup.setLogicOperator(CQLLogicalOperator.AND);
+			target.setGroup(finalgroup);
+			query.setTarget(target);
+			
+			List resultList = appService.query(query);
+			for (Iterator resultsIterator = resultList.iterator(); resultsIterator.hasNext();)
+			{
+				SubjectAssignment ii = (SubjectAssignment) resultsIterator.next();
+				for(Iterator i = ii.getStudySubjectIdentifier().iterator();i.hasNext();)
+				{	
+					II ident = (II)i.next();
+					labFm.setPatientId(ident.getExtension());
+					//System.out.println(ident.getExtension());
+				}
+				for(Iterator i = ii.getStudySite().getStudy().getStudyIdentifier().iterator();i.hasNext();)
+				{	
+					II ident = (II)i.next();
+					labFm.setStudyId(ident.getExtension());
+					//System.out.println(ident.getExtension());
+				}
+			}		
+		}
+		catch(Exception e){}
+		}	
 }
