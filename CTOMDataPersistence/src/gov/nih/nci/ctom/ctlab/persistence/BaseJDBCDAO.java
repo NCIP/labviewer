3916ab8ecbffea8c3e4be378a0ff1490e7507c6e
Unknown <unknown@unknown.com>
2007-11-28 14:17:03 +0000

    Changed the connection to get the connection from the hibernate connection pool using spring
    SVN-Revision: 199

--- CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/persistence/BaseJDBCDAO.java
+++ CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/persistence/BaseJDBCDAO.java
@@ -7,41 +7,26 @@
 package gov.nih.nci.ctom.ctlab.persistence;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
 /**
- * @author griffinch
+ * BaseJDBCDAO is just used to get the connection to the database. It uses spring to
+ * get the connection from the hibernate connection pool.
+ * <P>
+ * @author mholck
  */
 public abstract class BaseJDBCDAO
-{
-	
+{	
 	public Connection getConnection()
 	{
-	    Connection result = null;
-	    
-		String fDriverName="oracle.jdbc.driver.OracleDriver";
-		String fDbName="jdbc:oracle:thin:@cbiodb30.nci.nih.gov:1521:cccqa";
-		String fUserName="caxchange";
-		String fPassword="caxchange";
+		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("DAOConfig.xml"));
+		LabViewerDAOSupport dao = (LabViewerDAOSupport)factory.getBean("DAO");
+		Session session = dao.getSessionFactory().openSession();
 
-	    try
-	    {
-	       Class.forName(fDriverName).newInstance();
-	    }
-	    catch (Exception ex)
-	    {
-	        System.err.println("Check classpath. Cannot load db driver: " + fDriverName);
-	    }
-
-	    try
-	    {
-	        result = DriverManager.getConnection(fDbName, fUserName, fPassword);
-	    }
-	    catch (SQLException e)
-	    {
-	        System.err.println( "Driver loaded, but cannot connect to db: " + fDbName);
-	    }
+		Connection result = session.connection();
 	    
 	    return result;
 	}
