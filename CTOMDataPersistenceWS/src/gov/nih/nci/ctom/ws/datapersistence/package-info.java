a6afd122a81e0d20edbe6c48205578bdc327c5ea
Unknown <unknown@unknown.com>
2008-02-06 15:41:24 +0000

    Removed since no longer used
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@479 a393f10d-60de-4571-9f1a-620cac27c168

--- CTOMDataPersistenceWS/src/gov/nih/nci/ctom/ws/datapersistence/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ctom.nci.nih.gov/ws/datapersistence", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.nih.nci.ctom.ws.datapersistence;
