1e53db219486b607946ec66f0549735bab4d46f0
Unknown <unknown@unknown.com>
2008-02-18 17:23:57 +0000

    Changed the SimpleDateFormat  pattern for saveActivity
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@582 a393f10d-60de-4571-9f1a-620cac27c168

--- CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/persistence/CTLabDAO.java
+++ CTOMDataPersistence/src/gov/nih/nci/ctom/ctlab/persistence/CTLabDAO.java
@@ -704,8 +704,8 @@ public class CTLabDAO extends BaseJDBCDAO
 		ps.setString(3, String.valueOf(act.getStartDateOrig()));
 		ps.setString(4, String.valueOf(act.getStartDateOrig()));
 		try {
-			java.util.Date  startTmp = new SimpleDateFormat("yyyyMMddHHmm").parse(String.valueOf(act.getStartDateOrig()));
-			java.util.Date  stopTmp = new SimpleDateFormat("yyyyMMddHHmm").parse(String.valueOf(act.getStartDateOrig()));
+			java.util.Date  startTmp = new SimpleDateFormat("yyyyMMddHHmmss").parse(act.getStartDateOrig());
+			java.util.Date  stopTmp = new SimpleDateFormat("yyyyMMddHHmmss").parse(act.getStartDateOrig());
 			ps.setTimestamp(5,new java.sql.Timestamp(startTmp.getTime()));
 			ps.setTimestamp(6, new java.sql.Timestamp(stopTmp.getTime()));
 			ps.execute();
