fb3bfe4488bc6d9c4e0c91224bb13096cbe57a96
Unknown <unknown@unknown.com>
2008-03-29 16:51:10 +0000

    removed the unused java classes, added license to all the src files and removed unused methods
    SVN-Revision: 656

--- ctomlabviewer/src/gov/nih/nci/caxchange/ws/routingandworkflow/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://caxchange.nci.nih.gov/ws/routingandworkflow", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.nih.nci.caxchange.ws.routingandworkflow;
