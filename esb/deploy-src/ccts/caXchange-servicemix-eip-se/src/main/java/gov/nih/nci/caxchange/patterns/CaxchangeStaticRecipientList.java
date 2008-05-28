51bf91b5956c6f518f615b52f59ac9045d8a1eb0
Harsh Marwaha <harsh.marwaha1@gmail.com>
2008-05-28 18:16:44 +0000

    Fixed bug to return correct response in case of a time out at the aggregator.
    Bug: http://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=10659&group_id=411&atid=1609
    
    
    SVN-Revision: 704

--- esb/deploy-src/ccts/caXchange-servicemix-eip-se/src/main/java/gov/nih/nci/caxchange/patterns/CaxchangeStaticRecipientList.java
+++ esb/deploy-src/ccts/caXchange-servicemix-eip-se/src/main/java/gov/nih/nci/caxchange/patterns/CaxchangeStaticRecipientList.java
@@ -1,9 +1,14 @@
 package gov.nih.nci.caxchange.patterns;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.jbi.messaging.MessageExchange;
 
 import javax.jbi.messaging.NormalizedMessage;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.servicemix.eip.patterns.StaticRecipientList;
 
 /**
@@ -13,14 +18,36 @@ import org.apache.servicemix.eip.patterns.StaticRecipientList;
  *                  description="A static Recipient List that adds the count of recipients as a property"
  */
 public class CaxchangeStaticRecipientList extends StaticRecipientList {
-    
+	
+	Logger logger = LogManager.getLogger(CaxchangeStaticRecipientList.class);
+
+	 private List targetServiceIdentifiers;
+
 	/**
      * Default constructor
      */
 	public CaxchangeStaticRecipientList() {
     }
+
     /**
-     * This methods counts the number of static receipients 
+     * 
+     * @return list of target service identifiers this recipient list is routing to.
+     */
+    public List getTargetServiceIdentifiers() {
+		   return targetServiceIdentifiers;
+    }
+    /**
+     * 
+     * @param targetServiceIdentifiers
+     */
+    public void setTargetServiceIdentifiers(List targetServiceIdentifiers) {
+		   this.targetServiceIdentifiers = targetServiceIdentifiers;
+    }
+
+    /**
+     * This method  sets the message properties for the EIP aggregator component.
+     * The properties set are recipient count, correlation id, and target service identifiers.
+     * 
      * @param exchange
      * @return
      * @throws Exception
@@ -30,6 +57,28 @@ public class CaxchangeStaticRecipientList extends StaticRecipientList {
          int length= getRecipients().length;
          in.setProperty(CaxchangeEIPConstants.CAXCHANGE_RECIPIENT_COUNT, length+"");
          in.setProperty(CaxchangeEIPConstants.ORIGINAL_EXCHANGE_CORRELATIONID, exchange.getProperty("org.apache.servicemix.correlationId"));
+         in.setProperty(CaxchangeEIPConstants.CAXCHANGE_RECIPIENTS, getTargetServiceIdentifiersAsString());
          super.process(exchange);
-    }    
+    }
+    /**
+     * Creates a comma delimited string of the target service identifiers. This can
+     * be set as a property of the message.
+     * 
+     * @return a comma delimited string of the target service identifiers.
+     */
+    public String getTargetServiceIdentifiersAsString() {
+		if (targetServiceIdentifiers==null) return null;
+		StringBuffer identifiers = new StringBuffer(30);
+		Iterator identifierIterator = targetServiceIdentifiers.iterator();
+		while(identifierIterator.hasNext()) {
+			 Object o = identifierIterator.next();
+			 String serviceIdentifier = (String)o;
+	         identifiers.append(serviceIdentifier+",");
+	    }
+	    if (identifiers.length()>0) {
+			identifiers.deleteCharAt(identifiers.length()-1);
+	    }
+	    return identifiers.toString();
+
+    }
 }
