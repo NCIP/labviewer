f684a5e47cf9b2d8c832758e8bbd6b0a4587fd0d
Unknown <unknown@unknown.com>
2008-08-12 04:18:11 +0000

    postgres migration
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@942 a393f10d-60de-4571-9f1a-620cac27c168

---
+++ CTOMDataPersistence-PG/src/gov/nih/nci/ctom/ctlab/domain/Participant.java
@@ -0,0 +1,490 @@
+package gov.nih.nci.ctom.ctlab.domain;
+
+import java.util.Date;
+
+public class Participant {
+
+	private Long id;
+	private String initials = null;
+	private String lastName = null;
+	private String firstName = null;
+	private String middleName = null;
+	
+	private Date birthDate = null;
+	private String birthDateOrig = null;
+	private String adminGenderCode = null;
+	private String adminGenderCodeSystem = null;
+	private String adminGenderCodeSystemName = null;
+	
+	private String telecomAddress=null;
+	private String streetAddress = null;
+	private String city = null;
+	private String state = null;
+	private String zipCode = null;
+	private String countryCode;
+	private String phone = null;
+	
+	private String educationLevelCode =null;
+	
+	private String ethnicGroupCode=null;
+	private String householdIncomeCode=null;
+	private String maritalStatusCode;
+	private String raceCode;
+	private String raceCodeSystem;
+	private String raceCodeSystemName;
+	private String employmentStatusCode = null;
+	private String employmentStatusOtherTxt = null;
+	
+	private String paymentMethodCode=null;
+	private String confidentialityInd=null;
+	
+	private Long admGndrConceptDescId=null;
+	private Long raceConceptDescId=null;
+	
+	private Long securityKey=null;
+	private String source=null;
+	private Date sourceExtractDate =null;
+	private Date ctomInsertDate=null;
+	private Date ctomUpdateDate=null;
+	
+	private Identifier identifier;
+	
+	public Participant(){
+		identifier = new Identifier();
+	}
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the initials
+	 */
+	public String getInitials() {
+		return initials;
+	}
+	/**
+	 * @param initials the initials to set
+	 */
+	public void setInitials(String initials) {
+		this.initials = initials;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the middleName
+	 */
+	public String getMiddleName() {
+		return middleName;
+	}
+	/**
+	 * @param middleName the middleName to set
+	 */
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	/**
+	 * @return the birthDate
+	 */
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	/**
+	 * @param birthDate the birthDate to set
+	 */
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	/**
+	 * @return the birthDateOrig
+	 */
+	public String getBirthDateOrig() {
+		return birthDateOrig;
+	}
+	/**
+	 * @param birthDateOrig the birthDateOrig to set
+	 */
+	public void setBirthDateOrig(String birthDateOrig) {
+		this.birthDateOrig = birthDateOrig;
+	}
+	/**
+	 * @return the adminGenderCode
+	 */
+	public String getAdminGenderCode() {
+		return adminGenderCode;
+	}
+	/**
+	 * @param adminGenderCode the adminGenderCode to set
+	 */
+	public void setAdminGenderCode(String adminGenderCode) {
+		this.adminGenderCode = adminGenderCode;
+	}
+	/**
+	 * @return the telecomAddress
+	 */
+	public String getTelecomAddress() {
+		return telecomAddress;
+	}
+	/**
+	 * @param telecomAddress the telecomAddress to set
+	 */
+	public void setTelecomAddress(String telecomAddress) {
+		this.telecomAddress = telecomAddress;
+	}
+	/**
+	 * @return the streetAddress
+	 */
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	/**
+	 * @param streetAddress the streetAddress to set
+	 */
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+	/**
+	 * @param zipCode the zipCode to set
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	/**
+	 * @return the countryCode
+	 */
+	public String getCountryCode() {
+		return countryCode;
+	}
+	/**
+	 * @param countryCode the countryCode to set
+	 */
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the educationLevelCode
+	 */
+	public String getEducationLevelCode() {
+		return educationLevelCode;
+	}
+	/**
+	 * @param educationLevelCode the educationLevelCode to set
+	 */
+	public void setEducationLevelCode(String educationLevelCode) {
+		this.educationLevelCode = educationLevelCode;
+	}
+	/**
+	 * @return the ethnicGroupCode
+	 */
+	public String getEthnicGroupCode() {
+		return ethnicGroupCode;
+	}
+	/**
+	 * @param ethnicGroupCode the ethnicGroupCode to set
+	 */
+	public void setEthnicGroupCode(String ethnicGroupCode) {
+		this.ethnicGroupCode = ethnicGroupCode;
+	}
+	/**
+	 * @return the householdIncomeCode
+	 */
+	public String getHouseholdIncomeCode() {
+		return householdIncomeCode;
+	}
+	/**
+	 * @param householdIncomeCode the householdIncomeCode to set
+	 */
+	public void setHouseholdIncomeCode(String householdIncomeCode) {
+		this.householdIncomeCode = householdIncomeCode;
+	}
+	/**
+	 * @return the maritalStatusCode
+	 */
+	public String getMaritalStatusCode() {
+		return maritalStatusCode;
+	}
+	/**
+	 * @param maritalStatusCode the maritalStatusCode to set
+	 */
+	public void setMaritalStatusCode(String maritalStatusCode) {
+		this.maritalStatusCode = maritalStatusCode;
+	}
+	/**
+	 * @return the raceCode
+	 */
+	public String getRaceCode() {
+		return raceCode;
+	}
+	/**
+	 * @param raceCode the raceCode to set
+	 */
+	public void setRaceCode(String raceCode) {
+		this.raceCode = raceCode;
+	}
+	/**
+	 * @return the employmentStatusCode
+	 */
+	public String getEmploymentStatusCode() {
+		return employmentStatusCode;
+	}
+	/**
+	 * @param employmentStatusCode the employmentStatusCode to set
+	 */
+	public void setEmploymentStatusCode(String employmentStatusCode) {
+		this.employmentStatusCode = employmentStatusCode;
+	}
+	/**
+	 * @return the employmentStatusOtherTxt
+	 */
+	public String getEmploymentStatusOtherTxt() {
+		return employmentStatusOtherTxt;
+	}
+	/**
+	 * @param employmentStatusOtherTxt the employmentStatusOtherTxt to set
+	 */
+	public void setEmploymentStatusOtherTxt(String employmentStatusOtherTxt) {
+		this.employmentStatusOtherTxt = employmentStatusOtherTxt;
+	}
+	/**
+	 * @return the paymentMethodCode
+	 */
+	public String getPaymentMethodCode() {
+		return paymentMethodCode;
+	}
+	/**
+	 * @param paymentMethodCode the paymentMethodCode to set
+	 */
+	public void setPaymentMethodCode(String paymentMethodCode) {
+		this.paymentMethodCode = paymentMethodCode;
+	}
+	/**
+	 * @return the confidentialityInd
+	 */
+	public String getConfidentialityInd() {
+		return confidentialityInd;
+	}
+	/**
+	 * @param confidentialityInd the confidentialityInd to set
+	 */
+	public void setConfidentialityInd(String confidentialityInd) {
+		this.confidentialityInd = confidentialityInd;
+	}
+	/**
+	 * @return the admGndrConceptDescId
+	 */
+	public Long getAdmGndrConceptDescId() {
+		return admGndrConceptDescId;
+	}
+	/**
+	 * @param admGndrConceptDescId the admGndrConceptDescId to set
+	 */
+	public void setAdmGndrConceptDescId(Long admGndrConceptDescId) {
+		this.admGndrConceptDescId = admGndrConceptDescId;
+	}
+	/**
+	 * @return the raceConceptDescId
+	 */
+	public Long getRaceConceptDescId() {
+		return raceConceptDescId;
+	}
+	/**
+	 * @param raceConceptDescId the raceConceptDescId to set
+	 */
+	public void setRaceConceptDescId(Long raceConceptDescId) {
+		this.raceConceptDescId = raceConceptDescId;
+	}
+	/**
+	 * @return the securityKey
+	 */
+	public Long getSecurityKey() {
+		return securityKey;
+	}
+	/**
+	 * @param securityKey the securityKey to set
+	 */
+	public void setSecurityKey(Long securityKey) {
+		this.securityKey = securityKey;
+	}
+	/**
+	 * @return the source
+	 */
+	public String getSource() {
+		return source;
+	}
+	/**
+	 * @param source the source to set
+	 */
+	public void setSource(String source) {
+		this.source = source;
+	}
+	/**
+	 * @return the sourceExtractDate
+	 */
+	public Date getSourceExtractDate() {
+		return sourceExtractDate;
+	}
+	/**
+	 * @param sourceExtractDate the sourceExtractDate to set
+	 */
+	public void setSourceExtractDate(Date sourceExtractDate) {
+		this.sourceExtractDate = sourceExtractDate;
+	}
+	/**
+	 * @return the ctomInsertDate
+	 */
+	public Date getCtomInsertDate() {
+		return ctomInsertDate;
+	}
+	/**
+	 * @param ctomInsertDate the ctomInsertDate to set
+	 */
+	public void setCtomInsertDate(Date ctomInsertDate) {
+		this.ctomInsertDate = ctomInsertDate;
+	}
+	/**
+	 * @return the ctomUpdateDate
+	 */
+	public Date getCtomUpdateDate() {
+		return ctomUpdateDate;
+	}
+	/**
+	 * @param ctomUpdateDate the ctomUpdateDate to set
+	 */
+	public void setCtomUpdateDate(Date ctomUpdateDate) {
+		this.ctomUpdateDate = ctomUpdateDate;
+	}
+	/**
+	 * @return the identifier
+	 */
+	public Identifier getIdentifier() {
+		return identifier;
+	}
+	/**
+	 * @param identifier the identifier to set
+	 */
+	public void setIdentifier(Identifier identifier) {
+		this.identifier = identifier;
+	}
+	/**
+	 * @return the adminGenderCodeSystem
+	 */
+	public String getAdminGenderCodeSystem() {
+		return adminGenderCodeSystem;
+	}
+	/**
+	 * @param adminGenderCodeSystem the adminGenderCodeSystem to set
+	 */
+	public void setAdminGenderCodeSystem(String adminGenderCodeSystem) {
+		this.adminGenderCodeSystem = adminGenderCodeSystem;
+	}
+	/**
+	 * @return the adminGenderCodeSystemName
+	 */
+	public String getAdminGenderCodeSystemName() {
+		return adminGenderCodeSystemName;
+	}
+	/**
+	 * @param adminGenderCodeSystemName the adminGenderCodeSystemName to set
+	 */
+	public void setAdminGenderCodeSystemName(String adminGenderCodeSystemName) {
+		this.adminGenderCodeSystemName = adminGenderCodeSystemName;
+	}
+	/**
+	 * @return the raceCodeSystem
+	 */
+	public String getRaceCodeSystem() {
+		return raceCodeSystem;
+	}
+	/**
+	 * @param raceCodeSystem the raceCodeSystem to set
+	 */
+	public void setRaceCodeSystem(String raceCodeSystem) {
+		this.raceCodeSystem = raceCodeSystem;
+	}
+	/**
+	 * @return the raceCodeSystemName
+	 */
+	public String getRaceCodeSystemName() {
+		return raceCodeSystemName;
+	}
+	/**
+	 * @param raceCodeSystemName the raceCodeSystemName to set
+	 */
+	public void setRaceCodeSystemName(String raceCodeSystemName) {
+		this.raceCodeSystemName = raceCodeSystemName;
+	}
+	
+	
+	
+}
---
+++ CTOMDataPersistence-PG/src/gov/nih/nci/ctom/ctlab/domain/StudyParticipantAssignment.java
@@ -0,0 +1,365 @@
+package gov.nih.nci.ctom.ctlab.domain;
+
+import java.util.Date;
+
+public class StudyParticipantAssignment {
+
+	private Long id;
+	private Long studyPartId;
+	private String  studyPartIdOrig = null;
+	private String armId;
+	private String subGroupCode=null;
+	private Date informedConsetFrmSignedDt;
+	private String informedConsetFrmSignedDtOrg;
+	private Long enrollAge;
+	private String enrollAgeOrg;
+	private Long studyAgentDoseLevel;
+	private String studyAgentDoseLevelOrg;
+	private String studyAgentDoseLevelUOMCD;
+	private Date offStudyDt;
+	private String offStudyDtOrg;
+	private String offStudyResCode;
+	private String offStudyResOthTxt;
+	private String type;
+	private Long participantId;
+	private Long studySiteId;
+	private int securityKey;
+	private String source;
+	private Date srcExtractDt;
+	private Date ctomInsertDt;
+	private Date ctomUpdateDt;
+	private Activity activity=null;
+	private Participant participant=null;
+	private Identifier identifier;
+	
+	public StudyParticipantAssignment()
+	{
+		identifier = new Identifier();
+	}
+	
+	/**
+	 * @return the studyPartId
+	 */
+	public Long getStudyPartId() {
+		return studyPartId;
+	}
+	/**
+	 * @param studyPartId the studyPartId to set
+	 */
+	public void setStudyPartId(Long studyPartId) {
+		this.studyPartId = studyPartId;
+	}
+	/**
+	 * @return the armId
+	 */
+	public String getArmId() {
+		return armId;
+	}
+	/**
+	 * @param armId the armId to set
+	 */
+	public void setArmId(String armId) {
+		this.armId = armId;
+	}
+	/**
+	 * @return the subGroupCode
+	 */
+	public String getSubGroupCode() {
+		return subGroupCode;
+	}
+	/**
+	 * @param subGroupCode the subGroupCode to set
+	 */
+	public void setSubGroupCode(String subGroupCode) {
+		this.subGroupCode = subGroupCode;
+	}
+	/**
+	 * @return the informedConsetFrmSignedDt
+	 */
+	public Date getInformedConsetFrmSignedDt() {
+		return informedConsetFrmSignedDt;
+	}
+	/**
+	 * @param informedConsetFrmSignedDt the informedConsetFrmSignedDt to set
+	 */
+	public void setInformedConsetFrmSignedDt(Date informedConsetFrmSignedDt) {
+		this.informedConsetFrmSignedDt = informedConsetFrmSignedDt;
+	}
+	/**
+	 * @return the informedConsetFrmSignedDtOrg
+	 */
+	public String getInformedConsetFrmSignedDtOrg() {
+		return informedConsetFrmSignedDtOrg;
+	}
+	/**
+	 * @param informedConsetFrmSignedDtOrg the informedConsetFrmSignedDtOrg to set
+	 */
+	public void setInformedConsetFrmSignedDtOrg(String informedConsetFrmSignedDtOrg) {
+		this.informedConsetFrmSignedDtOrg = informedConsetFrmSignedDtOrg;
+	}
+	/**
+	 * @return the enrollAge
+	 */
+	public Long getEnrollAge() {
+		return enrollAge;
+	}
+	/**
+	 * @param enrollAge the enrollAge to set
+	 */
+	public void setEnrollAge(Long enrollAge) {
+		this.enrollAge = enrollAge;
+	}
+	/**
+	 * @return the enrollAgeOrg
+	 */
+	public String getEnrollAgeOrg() {
+		return enrollAgeOrg;
+	}
+	/**
+	 * @param enrollAgeOrg the enrollAgeOrg to set
+	 */
+	public void setEnrollAgeOrg(String enrollAgeOrg) {
+		this.enrollAgeOrg = enrollAgeOrg;
+	}
+	/**
+	 * @return the studyAgentDoseLevel
+	 */
+	public Long getStudyAgentDoseLevel() {
+		return studyAgentDoseLevel;
+	}
+	/**
+	 * @param studyAgentDoseLevel the studyAgentDoseLevel to set
+	 */
+	public void setStudyAgentDoseLevel(Long studyAgentDoseLevel) {
+		this.studyAgentDoseLevel = studyAgentDoseLevel;
+	}
+	/**
+	 * @return the studyAgentDoseLevelOrg
+	 */
+	public String getStudyAgentDoseLevelOrg() {
+		return studyAgentDoseLevelOrg;
+	}
+	/**
+	 * @param studyAgentDoseLevelOrg the studyAgentDoseLevelOrg to set
+	 */
+	public void setStudyAgentDoseLevelOrg(String studyAgentDoseLevelOrg) {
+		this.studyAgentDoseLevelOrg = studyAgentDoseLevelOrg;
+	}
+	/**
+	 * @return the studyAgentDoseLevelUOMCD
+	 */
+	public String getStudyAgentDoseLevelUOMCD() {
+		return studyAgentDoseLevelUOMCD;
+	}
+	/**
+	 * @param studyAgentDoseLevelUOMCD the studyAgentDoseLevelUOMCD to set
+	 */
+	public void setStudyAgentDoseLevelUOMCD(String studyAgentDoseLevelUOMCD) {
+		this.studyAgentDoseLevelUOMCD = studyAgentDoseLevelUOMCD;
+	}
+	/**
+	 * @return the offStudyDt
+	 */
+	public Date getOffStudyDt() {
+		return offStudyDt;
+	}
+	/**
+	 * @param offStudyDt the offStudyDt to set
+	 */
+	public void setOffStudyDt(Date offStudyDt) {
+		this.offStudyDt = offStudyDt;
+	}
+	/**
+	 * @return the offStudyDtOrg
+	 */
+	public String getOffStudyDtOrg() {
+		return offStudyDtOrg;
+	}
+	/**
+	 * @param offStudyDtOrg the offStudyDtOrg to set
+	 */
+	public void setOffStudyDtOrg(String offStudyDtOrg) {
+		this.offStudyDtOrg = offStudyDtOrg;
+	}
+	/**
+	 * @return the offStudyResCode
+	 */
+	public String getOffStudyResCode() {
+		return offStudyResCode;
+	}
+	/**
+	 * @param offStudyResCode the offStudyResCode to set
+	 */
+	public void setOffStudyResCode(String offStudyResCode) {
+		this.offStudyResCode = offStudyResCode;
+	}
+	/**
+	 * @return the offStudyResOthTxt
+	 */
+	public String getOffStudyResOthTxt() {
+		return offStudyResOthTxt;
+	}
+	/**
+	 * @param offStudyResOthTxt the offStudyResOthTxt to set
+	 */
+	public void setOffStudyResOthTxt(String offStudyResOthTxt) {
+		this.offStudyResOthTxt = offStudyResOthTxt;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the participantId
+	 */
+	public Long getParticipantId() {
+		return participantId;
+	}
+	/**
+	 * @param participantId the participantId to set
+	 */
+	public void setParticipantId(Long participantId) {
+		this.participantId = participantId;
+	}
+	/**
+	 * @return the studySiteId
+	 */
+	public Long getStudySiteId() {
+		return studySiteId;
+	}
+	/**
+	 * @param studySiteId the studySiteId to set
+	 */
+	public void setStudySiteId(Long studySiteId) {
+		this.studySiteId = studySiteId;
+	}
+	/**
+	 * @return the securityKey
+	 */
+	public int getSecurityKey() {
+		return securityKey;
+	}
+	/**
+	 * @param securityKey the securityKey to set
+	 */
+	public void setSecurityKey(int securityKey) {
+		this.securityKey = securityKey;
+	}
+	/**
+	 * @return the source
+	 */
+	public String getSource() {
+		return source;
+	}
+	/**
+	 * @param source the source to set
+	 */
+	public void setSource(String source) {
+		this.source = source;
+	}
+	/**
+	 * @return the srcExtractDt
+	 */
+	public Date getSrcExtractDt() {
+		return srcExtractDt;
+	}
+	/**
+	 * @param srcExtractDt the srcExtractDt to set
+	 */
+	public void setSrcExtractDt(Date srcExtractDt) {
+		this.srcExtractDt = srcExtractDt;
+	}
+	/**
+	 * @return the ctomInsertDt
+	 */
+	public Date getCtomInsertDt() {
+		return ctomInsertDt;
+	}
+	/**
+	 * @param ctomInsertDt the ctomInsertDt to set
+	 */
+	public void setCtomInsertDt(Date ctomInsertDt) {
+		this.ctomInsertDt = ctomInsertDt;
+	}
+	/**
+	 * @return the ctomUpdateDt
+	 */
+	public Date getCtomUpdateDt() {
+		return ctomUpdateDt;
+	}
+	/**
+	 * @param ctomUpdateDt the ctomUpdateDt to set
+	 */
+	public void setCtomUpdateDt(Date ctomUpdateDt) {
+		this.ctomUpdateDt = ctomUpdateDt;
+	}
+	/**
+	 * @return the studyPartIdOrig
+	 */
+	public String getStudyPartIdOrig() {
+		return studyPartIdOrig;
+	}
+	/**
+	 * @param studyPartIdOrig the studyPartIdOrig to set
+	 */
+	public void setStudyPartIdOrig(String studyPartIdOrig) {
+		this.studyPartIdOrig = studyPartIdOrig;
+	}
+	
+	/**
+	 * @return the activity
+	 */
+	public Activity getActivity() {
+		return activity;
+	}
+	/**
+	 * @param activity the activity to set
+	 */
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+	}
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the participant
+	 */
+	public Participant getParticipant() {
+		return participant;
+	}
+	/**
+	 * @param participant the participant to set
+	 */
+	public void setParticipant(Participant participant) {
+		this.participant = participant;
+	}
+	/**
+	 * @return the identifier
+	 */
+	public Identifier getIdentifier() {
+		return identifier;
+	}
+	/**
+	 * @param identifier the identifier to set
+	 */
+	public void setIdentifier(Identifier identifier) {
+		this.identifier = identifier;
+	}
+}
