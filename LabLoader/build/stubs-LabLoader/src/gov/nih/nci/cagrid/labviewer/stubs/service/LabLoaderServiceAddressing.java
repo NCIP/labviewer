99bc550ba31a42fcfab979e3c23d194d85876d15
Unknown <unknown@unknown.com>
2008-09-29 15:28:56 +0000

    moving to caGrid 1.2
    SVN-Revision: 1082

---
+++ LabLoader/build/stubs-LabLoader/src/gov/nih/nci/cagrid/labviewer/stubs/service/LabLoaderServiceAddressing.java
@@ -0,0 +1,14 @@
+/**
+ * LabLoaderServiceAddressing.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC2 Apr 28, 2006 (12:42:00 EDT) WSDL2Java emitter.
+ */
+
+package gov.nih.nci.cagrid.labviewer.stubs.service;
+
+public interface LabLoaderServiceAddressing extends gov.nih.nci.cagrid.labviewer.stubs.service.LabLoaderService {
+    public gov.nih.nci.cagrid.labviewer.stubs.LabLoaderPortType getLabLoaderPortTypePort(org.apache.axis.message.addressing.EndpointReferenceType reference) throws javax.xml.rpc.ServiceException;
+
+
+}
