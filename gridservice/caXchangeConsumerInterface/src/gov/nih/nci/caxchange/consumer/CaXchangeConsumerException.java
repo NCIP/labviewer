bc83edf831de9510776ee57ea063c4e50472bd07
Harsh Marwaha <harsh.marwaha1@gmail.com>
2008-10-14 13:12:20 +0000

    caXchange consumer interface.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@1101 a393f10d-60de-4571-9f1a-620cac27c168

---
+++ gridservice/caXchangeConsumerInterface/src/gov/nih/nci/caxchange/consumer/CaXchangeConsumerException.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.caxchange.consumer;
+
+import java.rmi.RemoteException;
+
+public class CaXchangeConsumerException extends RemoteException {
+
+	public CaXchangeConsumerException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CaXchangeConsumerException(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CaXchangeConsumerException(String s, Throwable cause) {
+		super(s, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
