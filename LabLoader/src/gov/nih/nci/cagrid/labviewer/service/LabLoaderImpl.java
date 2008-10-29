2cf6d43dda9cc179fdf2c4344c946d7cb5af20b1
Unknown <unknown@unknown.com>
2008-10-29 15:11:25 +0000

    performing authentication/authorization
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@1136 a393f10d-60de-4571-9f1a-620cac27c168

--- LabLoader/src/gov/nih/nci/cagrid/labviewer/service/LabLoaderImpl.java
+++ LabLoader/src/gov/nih/nci/cagrid/labviewer/service/LabLoaderImpl.java
@@ -28,9 +28,9 @@ public class LabLoaderImpl extends LabLoaderImplBase
   public void loadLab(java.lang.String string) throws RemoteException {
 	  logger.info("LabLoader loadLab method called.");
 	  
-	 /* // Authorization code : note No Specific exception for LabLoader
+	  // Authorization code : note No Specific exception for LabLoader
 		String username = LabLoaderAuthorization.getCallerIdentity();
-		
+		String user = "";
 		if (username == null)
 		{
 			logger.error("No user credentials provided");
@@ -42,15 +42,19 @@ public class LabLoaderImpl extends LabLoaderImplBase
 			logger.info("User who called was " + username);
 			
 			LabViewerAuthorizationHelper lvaHelper = new LabViewerAuthorizationHelper();
-			
-			boolean authorized = lvaHelper.isAuthorized(username);
+			if(username!=null){
+				int beginIndex = username.lastIndexOf("=");
+				int endIndex = username.length();
+				user = username.substring(beginIndex+1, endIndex);
+				}
+			boolean authorized = lvaHelper.isAuthorized(user);
 			
 			if (!authorized)
 			{
 				logger.error("User not authorized for this operation");
 				
 			}else
-			{*/
+			{
 				  // Now unmarshall the HL7v3 message
 				  HL7v3CtLabUnMarshaller unMarshaller = new HL7v3CtLabUnMarshaller();
 				  Object obj = null;
@@ -73,6 +77,7 @@ public class LabLoaderImpl extends LabLoaderImplBase
 				  }//end of try
 				  catch (Exception ex)
 				  {
+					  logger.debug(ex.getMessage());
 					  try
 					  {
 						  con.rollback();
@@ -91,9 +96,9 @@ public class LabLoaderImpl extends LabLoaderImplBase
 					  }
 					  catch(Exception ex){}
 				  }//end of finally
-/*	   }//end of else
    }//end of else
-*/ 
+   }//end of else
+ 
 }
 
   public void rollback(java.lang.String string) throws RemoteException {
