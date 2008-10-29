fa5c9009d880383c67d058c9a1b1a1f189d64b12
Unknown <unknown@unknown.com>
2008-10-29 14:38:05 +0000

    changes to include the grid credentials to invoke grid service
    SVN-Revision: 1130

--- Code/CancerCenterClient/src/java/main/gov/nih/nci/caxchange/client/CancerCenterClient.java
+++ Code/CancerCenterClient/src/java/main/gov/nih/nci/caxchange/client/CancerCenterClient.java
@@ -265,9 +265,9 @@ public class CancerCenterClient {
 	 */
 	public void process(ArrayList<ScheduledExecutorService>threadList) {
 
-		 HL7V3Transformation v3Transformation = new HL7V3Transformation(getInstance());
+		 /*HL7V3Transformation v3Transformation = new HL7V3Transformation(getInstance());
 		 v3Transformation.process(threadList);
-		
+		*/
 		 HL7V2ToHL7V3Tranformation v2Transformation = new HL7V2ToHL7V3Tranformation(getInstance());
 		 v2Transformation.process(threadList);
 		 
