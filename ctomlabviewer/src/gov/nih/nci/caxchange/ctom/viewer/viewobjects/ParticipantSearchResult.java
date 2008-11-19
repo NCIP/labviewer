102889d9797ab140240669d8d5cf98897938cc6c
Unknown <unknown@unknown.com>
2008-11-19 15:25:01 +0000

    filters for view objects
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@1184 a393f10d-60de-4571-9f1a-620cac27c168

--- ctomlabviewer/src/gov/nih/nci/caxchange/ctom/viewer/viewobjects/ParticipantSearchResult.java
+++ ctomlabviewer/src/gov/nih/nci/caxchange/ctom/viewer/viewobjects/ParticipantSearchResult.java
@@ -67,42 +67,99 @@ import gov.nih.nci.labhub.domain.LaboratoryResult;
 import gov.nih.nci.labhub.domain.SubjectAssignment;
 
 
-public class LabActivityResult
+public class ParticipantSearchResult
 {
 
-	private SubjectAssignment subjectAssignment = null;
-	private LaboratoryResult labResult = null;
-    private Date actualDate = null;
-	private String recordId = "";
+	
 	private String patientId = "";
 	private String studyId = "";
-	private String date = "";
-	private String labTestId = "";
-	private String textResult = "";
-	private String numericResult = "";
-	private String unitOfMeasure = "";
-	private String labResultId = "";;
-	private String lowRange = "";;
-	private String highRange = "";
-	private String isAdverseEvent = "";
-	private boolean adverseEventReported = false;
-	private String adverseEventReportedDate = "";
-	private boolean labLoadedToCDMS= false;
-	private String labLoadedToCDMSDate = "";
-	private String siteId ="";
-	
+	private String firstName="";
+    private String lastName="";
+    private String gridId="";
+    private String details="";
+    private String index;
 	/**
-	 * @return the siteId
+	 * @return the index
 	 */
-	public String getSiteId() {
-		return siteId;
+	public String getIndex() {
+		return index;
 	}
 
 	/**
-	 * @param siteId the siteId to set
+	 * @param index the index to set
 	 */
-	public void setSiteId(String siteId) {
-		this.siteId = siteId;
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	/**
+	 * @return the details
+	 */
+	public String getDetails() {
+		return details;
+	}
+
+	/**
+	 * @param details the details to set
+	 */
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	/**
+	 * @return the patientId
+	 */
+	public String getPatientId() {
+		return patientId;
+	}
+
+	/**
+	 * @param patientId the patientId to set
+	 */
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the gridId
+	 */
+	public String getGridId() {
+		return gridId;
+	}
+
+	/**
+	 * @param gridId the gridId to set
+	 */
+	public void setGridId(String gridId) {
+		this.gridId = gridId;
 	}
 
 	public String getStudyId()
@@ -115,194 +172,5 @@ public class LabActivityResult
 		this.studyId = studyId;
 	}
 
-	public LaboratoryResult getLabResult()
-	{
-		return labResult;
-	}
-
-	public void setLabResult(LaboratoryResult labResult)
-	{
-		this.labResult = labResult;
-	}
-
-	public String getLabResultId()
-	{
-		return labResultId;
-	}
-
-	public void setLabResultId(String labResultId)
-	{
-		this.labResultId = labResultId;
-	}
-
-	public String getRecordId()
-	{
-		return recordId;
-	}
-
-	public void setRecordId(String recordId)
-	{
-		this.recordId = recordId;
-	}
-
-	public String getDate()
-	{
-		return date;
-	}
-	
-	public void setDate(String date)
-	{
-		this.date = date;
-	}
-	
-	public String getHighRange()
-	{
-		return highRange;
-	}
-	
-	public void setHighRange(String highRange)
-	{
-		this.highRange = highRange;
-	}
-	
-	public String getLabTestId()
-	{
-		return labTestId;
-	}
-	
-	public void setLabTestId(String labTestId)
-	{
-		this.labTestId = labTestId;
-	}
-	
-	public String getLowRange()
-	{
-		return lowRange;
-	}
-	
-	public void setLowRange(String lowRange)
-	{
-		this.lowRange = lowRange;
-	}
-	
-	public String getNumericResult()
-	{
-		return numericResult;
-	}
-	
-	public void setNumericResult(String numericResult)
-	{
-		this.numericResult = numericResult;
-	}
-	
-	public String getPatientId()
-	{
-		return patientId;
-	}
-	
-	public void setPatientId(String patientId)
-	{
-		this.patientId = patientId;
-	}
-	
-	public SubjectAssignment getSubjectAssignment()
-	{
-		return subjectAssignment;
-	}
-	
-	public void setSubjectAssignment(SubjectAssignment subjectAssignment)
-	{
-		this.subjectAssignment = subjectAssignment;
-	}
-	
-	public String getTextResult()
-	{
-		return textResult;
-	}
-	
-	public void setTextResult(String textResult)
-	{
-		this.textResult = textResult;
-	}
-	
-	public String getUnitOfMeasure()
-	{
-		return unitOfMeasure;
-	}
-	
-	public void setUnitOfMeasure(String unitOfMeasure)
-	{
-		this.unitOfMeasure = unitOfMeasure;
-	}
-
-	public String getAdverseEventReportedDate()
-	{
-		return adverseEventReportedDate;
-	}
-
-	
-	public void setAdverseEventReportedDate(String adverseEventReportedDate)
-	{
-		this.adverseEventReportedDate = adverseEventReportedDate;
-	}
-
-	public String getIsAdverseEvent() {
-		return isAdverseEvent;
-	}
-
-	public void setIsAdverseEvent(String isAdverseEvent) {
-		this.isAdverseEvent = isAdverseEvent;
-	}
-
-	public void setActualDate(Date actualDate) {
-		this.actualDate = actualDate;
-		
-	}
-
-	public Date getActualDate() {
-		return actualDate;
-	}
-
-	/**
-	 * @return the labLoadedToCDMS
-	 */
-	public boolean isLabLoadedToCDMS() {
-		return labLoadedToCDMS;
-	}
-
-	/**
-	 * @param labLoadedToCDMS the labLoadedToCDMS to set
-	 */
-	public void setLabLoadedToCDMS(boolean labLoadedToCDMS) {
-		this.labLoadedToCDMS = labLoadedToCDMS;
-	}
-
-	/**
-	 * @return the labLoadedToCDMSDate
-	 */
-	public String getLabLoadedToCDMSDate() {
-		return labLoadedToCDMSDate;
-	}
-
-	/**
-	 * @param labLoadedToCDMSDate the labLoadedToCDMSDate to set
-	 */
-	public void setLabLoadedToCDMSDate(String labLoadedToCDMSDate) {
-		this.labLoadedToCDMSDate = labLoadedToCDMSDate;
-	}
-
-	/**
-	 * @return the adverseEventReported
-	 */
-	public boolean isAdverseEventReported() {
-		return adverseEventReported;
-	}
-
-	/**
-	 * @param adverseEventReported the adverseEventReported to set
-	 */
-	public void setAdverseEventReported(boolean adverseEventReported) {
-		this.adverseEventReported = adverseEventReported;
-	}
 	
 }
