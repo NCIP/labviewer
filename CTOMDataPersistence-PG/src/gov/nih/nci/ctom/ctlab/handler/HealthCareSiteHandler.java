bd3fb9a605b312821a7f5acc796148924115fb47
Unknown <unknown@unknown.com>
2009-03-16 14:28:54 +0000

    updated the persist method to include more information about the hcs and investigator
    SVN-Revision: 1405

--- CTOMDataPersistence-PG/src/gov/nih/nci/ctom/ctlab/handler/HealthCareSiteHandler.java
+++ CTOMDataPersistence-PG/src/gov/nih/nci/ctom/ctlab/handler/HealthCareSiteHandler.java
@@ -150,16 +150,29 @@ public class HealthCareSiteHandler extends CTLabDAO implements HL7V3MessageHandl
 				// insert into HealthCare_Site
 				ps =
 						con
-								.prepareStatement("insert into HEALTHCARE_SITE (ID, NCI_INSTITUTE_CODE, NAME, CTOM_INSERT_DATE)  values(?,?,?,?)");
+								.prepareStatement("insert into HEALTHCARE_SITE (ID, NCI_INSTITUTE_CODE, NAME, STREET_ADDRESS, CITY, STATE_CODE, POSTAL_CODE, COUNTRY_CODE, TELECOM_ADDRESS, SOURCE, SOURCE_EXTRACT_DATE, CTOM_UPDATE_DATE)  values(?,?,?,?,?,?,?,?,?,?,?,?)");
 
 				ps.setLong(1, hsId);
 				ps.setString(2, hcSite.getNciInstituteCd() != null ? hcSite.getNciInstituteCd()
 						: "");
-				ps.setString(3, hcSite.getNciInstituteCd() != null ? hcSite.getNciInstituteCd()
-						: "");
-				Date insertDt =
-						hcSite.getCtomInsertDt() != null ? hcSite.getCtomInsertDt() : new Date();
-				ps.setDate(4, new java.sql.Date(insertDt.getTime()));
+				ps.setString(3, String.valueOf(hcSite.getName() != null ? hcSite.getName() : ""));
+				ps.setString(4, String.valueOf(hcSite.getStreetAddr() != null ? hcSite.getStreetAddr() : ""));
+				ps.setString(5, String.valueOf(hcSite.getCity() != null ? hcSite.getCity() : ""));
+				ps.setString(6, String.valueOf(hcSite.getStateCode() != null ? hcSite.getStateCode() : ""));
+				ps.setString(7, String.valueOf(hcSite.getPostalCode() != null ? hcSite.getPostalCode() : ""));
+				ps.setString(8, String.valueOf(hcSite.getCountryCode() != null ? hcSite.getCountryCode() : ""));
+				ps.setString(9, String.valueOf(hcSite.getTelecomAddr() != null ? hcSite.getTelecomAddr() : ""));
+				ps.setString(10, String.valueOf(hcSite.getSource() != null ? hcSite.getSource() : ""));
+				Date srcExtractDt =
+					hcSite.getSrcExtractDt() != null ? hcSite.getSrcExtractDt() : new Date();
+			    
+					ps.setDate(11, new java.sql.Date(srcExtractDt.getTime()));
+				
+			    Date ctomUpdateDt =
+			    	hcSite.getCtomUpdateDt() != null ? hcSite.getCtomUpdateDt() : new Date();
+			    
+					ps.setDate(12, new java.sql.Date(ctomUpdateDt.getTime()));
+								
 				ps.execute();
 			}
 			// set the id
@@ -228,4 +241,125 @@ public class HealthCareSiteHandler extends CTLabDAO implements HL7V3MessageHandl
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gov.nih.nci.ctom.ctlab.handler.HL7V3MessageHandler#persist(java.sql.Connection,
+	 *      gov.nih.nci.ctom.ctlab.domain.Protocol)
+	 */
+	/*public void persist1(Connection con, Protocol protocol) throws Exception
+	{
+
+		logger.debug("Saving the HealthCareSite");
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Long hsId = null;
+		Long ssId = null;
+
+		// retrieve HealthCareSite from Protocol
+		HealthCareSite hcSite = protocol.getHealthCareSite();
+		Long protId = protocol.getId();
+
+		try
+		{
+			// check if there exists a healthcare site with the NCI Institute
+			// code
+			ps =
+					con
+							.prepareStatement("select id from healthcare_site where NCI_INSTITUTE_CODE = ?");
+			ps.setString(1, hcSite.getNciInstituteCd());
+			rs = ps.executeQuery();
+			if (rs.next())
+			{
+				// use the existing Id
+				hsId = rs.getLong(1);
+			}
+			else
+			{
+
+				// new HealthCare Site - Get Id from sequence
+				hsId = getNextVal(con, "ORGANIZATION_SEQ");
+				
+				//clean up
+				ps = SQLHelper.closePreparedStatement(ps);
+				
+				// insert into HealthCare_Site - source - CTEP ID :source_extract_date - the date it was updated by invoking COPPA service
+				ps =
+						con
+								.prepareStatement("insert into HEALTHCARE_SITE (ID, SOURCE, CTOM_INSERT_DATE)  values(?,?,?)");
+
+				ps.setLong(1, hsId);
+				ps.setString(2, hcSite.getSource()!= null ? hcSite.getSource(): "");
+				ps.setString(3, hcSite.getNciInstituteCd() != null ? hcSite.getNciInstituteCd()
+						: "");
+				Date insertDt =
+						hcSite.getCtomInsertDt() != null ? hcSite.getCtomInsertDt() : new Date();
+				ps.setDate(3, new java.sql.Date(insertDt.getTime()));
+				ps.execute();
+			}
+			// set the id
+			hcSite.setId(hsId);
+
+			// Check if the Study Site exits for that healthcaresite Id and
+			// Protocol Id
+			
+			//clean up
+			rs = SQLHelper.closeResultSet(rs);
+			ps = SQLHelper.closePreparedStatement(ps);
+			
+			ps =
+					con
+							.prepareStatement("select id from study_site where HEALTHCARE_SITE_ID = ? and PROTOCOL_ID = ?");
+			ps.setLong(1, hsId);
+			ps.setLong(2, protId);
+			rs = ps.executeQuery();
+			if (rs.next())
+			{
+				// If exists - use that Study site Id
+				ssId = rs.getLong(1);
+			}
+			else
+			{
+				// create a new Study Site id
+
+				// get the next Id value
+				ssId = getNextVal(con, "STUDY_SITE_SEQ");
+				
+				//clean up
+				ps = SQLHelper.closePreparedStatement(ps);
+				
+				// insert into Study_Site
+				ps =
+						con
+								.prepareStatement("insert into study_site (ID, HEALTHCARE_SITE_ID, PROTOCOL_ID)  values(?,?,?)");
+
+				ps.setLong(1, ssId);
+				ps.setLong(2, hsId);
+				ps.setLong(3, protId);
+				ps.execute();
+
+			}
+
+		}
+		catch (SQLException se)
+		{
+			logger.error("Error saving the HealthCareSite",se);
+			throw (new Exception(se.getLocalizedMessage()));
+
+		}
+		finally
+		{
+			//clean up
+			rs = SQLHelper.closeResultSet(rs);
+			ps = SQLHelper.closePreparedStatement(ps);
+
+		}
+		if (hcSite.getStudyParticipantAssignment() != null)
+		{
+			protocol.getHealthCareSite().getStudyParticipantAssignment().setStudySiteId(ssId);
+			// saves the StudyParticipant Assignment
+			HL7V3MessageHandlerFactory.getInstance().getHandler("STUDY_PARTICIPANT_ASSIGNMENT")
+					.persist(con, protocol);
+		}
+	}*/
 }
--- CTOMDataPersistence-PG/src/gov/nih/nci/ctom/ctlab/handler/InvestigatorHandler.java
+++ CTOMDataPersistence-PG/src/gov/nih/nci/ctom/ctlab/handler/InvestigatorHandler.java
@@ -89,6 +89,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 
@@ -142,13 +143,31 @@ public class InvestigatorHandler extends CTLabDAO implements HL7V3MessageHandler
 				// insert into Investigator
 				ps =
 						con
-								.prepareStatement("insert into investigator (ID, NCI_IDENTIFIER, LAST_NAME, FIRST_NAME)  values(?,?,?,?)");
+								.prepareStatement("insert into investigator (ID, NCI_IDENTIFIER, LAST_NAME, FIRST_NAME, MIDDLE_NAME, TELECOM_ADDRESS, STREET_ADDRESS, CITY, STATE, ZIP_CODE, COUNTRY_CODE, PHONE, SOURCE, SOURCE_EXTRACT_DATE, CTOM_UPDATE_DATE)  values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
 				ps.setLong(1, investigatorId);
 				ps.setString(2, String.valueOf(inv.getNciId() != null ? inv.getNciId() : ""));
 				ps.setString(3, String.valueOf(inv.getLastName() != null ? inv.getLastName() : ""));
-				ps.setString(4, String
-						.valueOf(inv.getFirstName() != null ? inv.getFirstName() : ""));
+				ps.setString(4, String.valueOf(inv.getFirstName() != null ? inv.getFirstName() : ""));
+				ps.setString(5, String.valueOf(inv.getMiddleNAle() != null ? inv.getMiddleNAle() : ""));
+				ps.setString(6, String.valueOf(inv.getTelecomAddr() != null ? inv.getTelecomAddr() : ""));
+				ps.setString(7, String.valueOf(inv.getStreetAddr() != null ? inv.getStreetAddr() : ""));
+				ps.setString(8, String.valueOf(inv.getCity() != null ? inv.getCity() : ""));
+				ps.setString(9, String.valueOf(inv.getState() != null ? inv.getState() : ""));
+				ps.setString(10, String.valueOf(inv.getZipCode() != null ? inv.getZipCode() : ""));
+				ps.setString(11, String.valueOf(inv.getCountryCode() != null ? inv.getCountryCode() : ""));
+				ps.setString(12, String.valueOf(inv.getPhone() != null ? inv.getPhone() : ""));
+				ps.setString(13, String.valueOf(inv.getSource() != null ? inv.getSource() : ""));
+				Date srcExtractDt =
+					inv.getSrcExtractDt() != null ? inv.getSrcExtractDt() : new Date();
+			    
+					ps.setDate(14, new java.sql.Date(srcExtractDt.getTime()));
+				
+			    Date ctomUpdateDt =
+					inv.getCtomUpdateDt() != null ? inv.getCtomUpdateDt() : new Date();
+			    
+					ps.setDate(15, new java.sql.Date(ctomUpdateDt.getTime()));
+				
 				ps.execute();
 
 			}
@@ -199,5 +218,102 @@ public class InvestigatorHandler extends CTLabDAO implements HL7V3MessageHandler
 
 		}
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gov.nih.nci.ctom.ctlab.handler.HL7V3MessageHandler#persist(java.sql.Connection,
+	 *      gov.nih.nci.ctom.ctlab.domain.Protocol)
+	 */
+	/*public void persist1(Connection con, Protocol protocol) throws Exception
+	{
+
+		logger.debug("Saving the Investigator");
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Long investigatorId = null;
+
+		// retrieve the investigator from Protocol
+		Investigator inv = protocol.getInvestigator();
+		Long protId = protocol.getId();
+
+		try
+		{
+			// Check if the investigator exists in the db -use the Id.
+			ps = con.prepareStatement("select ID from INVESTIGATOR  where NCI_IDENTIFIER = ?");
+			ps.setString(1, inv.getNciId());
+			rs = ps.executeQuery();
+			if (rs.next())
+			{
+				investigatorId = rs.getLong(1);
+			}
+			else
+			{
+				// clean up
+				ps = SQLHelper.closePreparedStatement(ps);
+	
+				// Get Id from sequence
+				investigatorId = getNextVal(con, "PERSON_SEQ");
+
+				// insert into Investigator -source - CTEP ID :source_extract_date - the date it was updated by invoking COPPA service
+				ps =
+						con
+								.prepareStatement("insert into investigator (ID, SOURCE, CTOM_INSERT_DATE)  values(?,?,?)");
+
+				ps.setLong(1, investigatorId);
+				ps.setString(2, inv.getSource() != null ? inv.getSource() : "");
+				Date insertDt =
+					inv.getCtomInsertDt() != null ? inv.getCtomInsertDt() : new Date();
+			    ps.setDate(3, new java.sql.Date(insertDt.getTime()));
+				ps.execute();
+
+			}
+			
+			//clean up
+			rs = SQLHelper.closeResultSet(rs);
+			ps = SQLHelper.closePreparedStatement(ps);
+			
+			// insert into Study_Investigator
+			ps =
+					con
+							.prepareStatement("select id from study_investigator where PROTOCOL_ID = ? AND INVESTIGATOR_ID = ?");
+			ps.setLong(1, protId);
+			ps.setLong(2, investigatorId);
+			rs = ps.executeQuery();
+			if (rs.next())
+			{
+				return;
+			}
+			else
+			{
+				//clean up
+				 ps = SQLHelper.closePreparedStatement(ps);
+				
+				Long siId = getNextVal(con, "STUDY_INVESTIGATOR_SEQ");
+				logger.debug("The study_Invest id is " + siId);
+				ps =
+						con
+								.prepareStatement("insert into study_investigator (ID, PROTOCOL_ID, INVESTIGATOR_ID)  values(?,?,?)");
+
+				ps.setLong(1, siId);
+				ps.setLong(2, protId);
+				ps.setLong(3, investigatorId);
+				ps.execute();
+			}
+		}
+		catch (SQLException se)
+		{
+			logger.error("Error saving the investigator",se);
+			throw (new Exception(se.getLocalizedMessage()));
+
+		}
+		finally
+		{
+			//clean up
+			rs = SQLHelper.closeResultSet(rs);
+			ps = SQLHelper.closePreparedStatement(ps);
+
+		}
+	}*/
 
 }
