b3709a9a458c8f953e96a49771c85e3545b50a1b
Ajay Nalamala <analamala@ekagrasoft.com>
2009-04-28 20:37:58 +0000

    Files removed because plugin added to generate the stubs using maven targets.
    SVN-Revision: 1510

--- esb/deploy-src/ccts/caXchange-cxf-stubs-jar/src/main/java/gov/nih/nci/caxchange/messaging/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://caXchange.nci.nih.gov/messaging", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.nih.nci.caxchange.messaging;
