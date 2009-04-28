9fbfe11f4832a3a8a37f0f1844fd0eacc3f010fc
Ajay Nalamala <analamala@ekagrasoft.com>
2009-04-28 20:35:10 +0000

    Sample files provided for easy testing of the CXF components.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@1509 a393f10d-60de-4571-9f1a-620cac27c168

---
+++ esb/deploy-src/ccts/caXchange-cxf-stubs-jar/src/main/java/gov/nih/nci/caxchange/cxfsamples/SampleAsyncHandler.java
@@ -0,0 +1,24 @@
+package gov.nih.nci.caxchange.cxfsamples;
+import gov.nih.nci.caxchange.messaging.ResponseMessage;
+
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
+
+
+public class SampleAsyncHandler implements AsyncHandler<ResponseMessage> {
+	
+	private ResponseMessage reply; 
+
+	public void handleResponse(Response<ResponseMessage> response) {
+		try{
+			reply = response.get();
+		} catch (Exception e){
+			e.printStackTrace();
+		}		
+	}
+	
+	public gov.nih.nci.caxchange.messaging.Response getResponse(){
+		return reply.getResponse();
+	}
+
+}
