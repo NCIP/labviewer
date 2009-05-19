7ccb8f5786e0b2429278c728ea5c61b61e2119c5
Ajay Nalamala <analamala@ekagrasoft.com>
2009-05-19 01:58:44 +0000

    Cleaned up the document. Commented blocks removed.
    SVN-Revision: 1555

--- Code/esb/deploy-src/ccts/caXchange-beans-su/src/main/java/gov/nih/nci/caxchange/servicemix/bean/util/ServerPasswordCallback.java
+++ Code/esb/deploy-src/ccts/caXchange-beans-su/src/main/java/gov/nih/nci/caxchange/servicemix/bean/util/ServerPasswordCallback.java
@@ -6,23 +6,40 @@ import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.ws.security.WSPasswordCallback;
 
+/**
+ * This is a callback class for handling the WS Security
+ * actions set in the WSS4JInInterceptor.
+ * 
+ * @author Ekagra Software Technologies
+ * 
+ */
 public class ServerPasswordCallback implements CallbackHandler {
 
+	static Logger logger = LogManager.getLogger(ServerPasswordCallback.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.security.auth.callback.CallbackHandler#handle(javax.security.auth
+	 * .callback.Callback[])
+	 */
 	public void handle(Callback[] callbacks) throws IOException,
 			UnsupportedCallbackException {
-		System.out
-				.println("**********SERVER PASSWORD VALIDATOR BEAN CALLED**********");
+		logger
+				.debug("**********SERVER PASSWORD VALIDATOR BEAN CALLED**********");
 
 		WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
-		System.out.println("User Identifier: " + pc.getIdentifer());
+		logger.debug("User Identifier: " + pc.getIdentifer());
 
 		if (pc.getIdentifer().equals("wsClient")) {
-			System.out.println("USERNAME PASSWORD SET BLOCK");
-			System.out.println("PASSWORD: " + pc.getPassword());
-			// set the password on the callback. This will be compared to the
-			// password which was sent from the client.
+			// for password digest, set the password on the callback.
+			// This will be compared to the password which was sent from the
+			// client.
 			// pc.setPassword("password");
 			if (!pc.getPassword().equals("password")) {
 				throw new IOException("wrong password");
