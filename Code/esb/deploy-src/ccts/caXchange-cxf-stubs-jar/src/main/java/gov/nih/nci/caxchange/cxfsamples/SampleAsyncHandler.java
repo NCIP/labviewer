4c953af66d5c734c9dbac286d511b62345198dc5
Ajay Nalamala <analamala@ekagrasoft.com>
2009-05-19 14:37:49 +0000

    Added comments
    SVN-Revision: 1561

--- Code/esb/deploy-src/ccts/caXchange-cxf-stubs-jar/src/main/java/gov/nih/nci/caxchange/cxfsamples/SampleAsyncHandler.java
+++ Code/esb/deploy-src/ccts/caXchange-cxf-stubs-jar/src/main/java/gov/nih/nci/caxchange/cxfsamples/SampleAsyncHandler.java
@@ -1,23 +1,33 @@
 package gov.nih.nci.caxchange.cxfsamples;
+
 import gov.nih.nci.caxchange.messaging.ResponseMessage;
 
 import javax.xml.ws.AsyncHandler;
 import javax.xml.ws.Response;
 
-
+/**
+ * This is a handler class for handling Asynchronous responses
+ * 
+ * @author Ekagra Software Technologies
+ * 
+ */
 public class SampleAsyncHandler implements AsyncHandler<ResponseMessage> {
-	
-	private ResponseMessage reply; 
 
+	private ResponseMessage reply;
+
+	/**
+	 * Gets called when the response arrives during 
+	 * asynchronous processing.
+	 */
 	public void handleResponse(Response<ResponseMessage> response) {
-		try{
+		try {
 			reply = response.get();
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
-		}		
+		}
 	}
-	
-	public gov.nih.nci.caxchange.messaging.Response getResponse(){
+
+	public gov.nih.nci.caxchange.messaging.Response getResponse() {
 		return reply.getResponse();
 	}
 
