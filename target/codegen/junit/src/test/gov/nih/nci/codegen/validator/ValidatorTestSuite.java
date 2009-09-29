265a1a8da06d70688ff29d2b62e0323b7d402798
Unknown <unknown@unknown.com>
2009-09-29 19:36:03 +0000

    BDA related changes to labviewer.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@1613 a393f10d-60de-4571-9f1a-620cac27c168

---
+++ target/codegen/junit/src/test/gov/nih/nci/codegen/validator/ValidatorTestSuite.java
@@ -0,0 +1,11 @@
+package test.gov.nih.nci.codegen.validator;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	PKGeneratorValidatorTest.class
+})
+public class ValidatorTestSuite{
+}
