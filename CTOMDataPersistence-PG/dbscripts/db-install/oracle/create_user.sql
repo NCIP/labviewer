2a196f51887059fddc0b60274806ea9b05a5dba6
Ram Bhattaru <rambhattaru@gmail.com>
2010-04-20 16:16:10 +0000

    Support for Oracle is added
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@2075 a393f10d-60de-4571-9f1a-620cac27c168

--- CTOMDataPersistence-PG/dbscripts/db-install/oracle/create_user.sql
+++ CTOMDataPersistence-PG/dbscripts/db-install/oracle/create_user.sql
@@ -1,11 +1,11 @@
-CREATE USER caxchange IDENTIFIED BY caxchange DEFAULT TABLESPACE USERS;
-GRANT CREATE SESSION TO caxchange;
-GRANT CREATE TABLE TO caxchange;
-GRANT CREATE VIEW TO caxchange;
-GRANT CREATE SEQUENCE TO caxchange; 
-GRANT CREATE SYNONYM TO caxchange;
-GRANT CREATE TRIGGER TO caxchange;
-ALTER USER caxchange QUOTA unlimited ON USERS;
+CREATE USER ctodslabviewer_dev IDENTIFIED BY j83oihsa DEFAULT TABLESPACE USERS;
+GRANT CREATE SESSION TO ctodslabviewer_dev;
+GRANT CREATE TABLE TO ctodslabviewer_dev;
+GRANT CREATE VIEW TO ctodslabviewer_dev;
+GRANT CREATE SEQUENCE TO ctodslabviewer_dev; 
+GRANT CREATE SYNONYM TO ctodslabviewer_dev;
+GRANT CREATE TRIGGER TO ctodslabviewer_dev;
+ALTER USER ctodslabviewer_dev QUOTA unlimited ON USERS;
 exit
 
 
