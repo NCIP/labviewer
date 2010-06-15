100c5bada276892a024146fcaa8a726396c37329
Unknown <unknown@unknown.com>
2010-06-15 14:30:30 +0000

    fixed lab_hub_organization view
    SVN-Revision: 2121

--- CTOMDataPersistence-PG/dbscripts/db-install/postgresql/PG_Views.sql
+++ CTOMDataPersistence-PG/dbscripts/db-install/postgresql/PG_Views.sql
@@ -200,7 +200,12 @@ UNION
 SELECT p.id               AS   id,
        p.identifier       As   identifier,
        p.name             AS   name
-FROM   performing_laboratory AS p;
+FROM   performing_laboratory AS p
+UNION
+SELECT h.id                 AS   id,
+       h.nci_institute_code As   identifier,
+       h.name               AS   name
+FROM   healthcare_site AS h;
 
 
 CREATE OR REPLACE VIEW LAB_HUB_PERSON
