c271fff0428535f9c4d1786ba6301594ab655f34
Unknown <unknown@unknown.com>
2010-07-09 17:26:48 +0000

    changed study consumer authorization as directed by suite, which affected authorization in all the services
    SVN-Revision: 2210

--- LabLoader-1.3/src/gov/nih/nci/cagrid/labviewer/service/LabLoaderImpl.java
+++ LabLoader-1.3/src/gov/nih/nci/cagrid/labviewer/service/LabLoaderImpl.java
@@ -2,6 +2,7 @@ package gov.nih.nci.cagrid.labviewer.service;
 
 import gov.nih.nci.cabig.ctms.suite.authorization.SuiteAuthorizationAccessException;
 import gov.nih.nci.cabig.ctms.suite.authorization.SuiteRole;
+import gov.nih.nci.cabig.ctms.suite.authorization.SuiteRoleMembership;
 import gov.nih.nci.cagrid.labviewer.service.globus.LabLoaderAuthorization;
 import gov.nih.nci.cagrid.labviewer.xml.HL7v3CtLabUnMarshaller;
 import gov.nih.nci.caxchange.ctom.viewer.util.LabViewerAuthorizationHelper;
@@ -9,8 +10,7 @@ import gov.nih.nci.ctom.ctlab.handler.ProtocolHandler;
 
 import java.rmi.RemoteException;
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
@@ -102,35 +102,62 @@ public class LabLoaderImpl extends LabLoaderImplBase
 	{
 		if (callerId == null)
 		{
-			log.error("Error saving lab - no user credentials provided");
+			log.error("Error saving lab: no user credentials provided");
 			throw new RemoteException("No user credentials provided");
 		}
 
-		log.debug("Service called by: " + callerId);
-		
+		log.debug("Service called by: " + callerId);		
 		int beginIndex = callerId.lastIndexOf("=") + 1;
 		int endIndex = callerId.length();
 		String username = callerId.substring(beginIndex, endIndex);
 		
-		HL7v3CtLabUnMarshaller unMarshaller = new HL7v3CtLabUnMarshaller();
-		String studyId = unMarshaller.getStudyId(xml);
-		if (studyId == null)
-		{
-			log.error("Error saving lab - no study identifier provided");
-			throw new RemoteException("No study identifier provided");
-		}
-		
-		List<String> siteNciInstituteCodes = new ArrayList<String>();
-		siteNciInstituteCodes.add(unMarshaller.getSiteNciInstituteCode(xml));
-		if (siteNciInstituteCodes.isEmpty())
-		{
-			log.error("Error saving lab - no site NCI institure codes provided");
-			throw new RemoteException("No site NCI institure codes provided");
-		}
-		
 		try
 		{
-		    getAuthorizationHelper().checkAuthorization(username, SuiteRole.LAB_DATA_USER, studyId, siteNciInstituteCodes);
+		    Map<SuiteRole, SuiteRoleMembership> userRoleMemberships = getAuthorizationHelper().getUserRoleMemberships(username);
+		    SuiteRole labLoaderRole = SuiteRole.LAB_DATA_USER;
+		    if (userRoleMemberships.containsKey(labLoaderRole))
+		    {
+		    	if (labLoaderRole.isScoped())
+		    	{
+		    		SuiteRoleMembership userRoleMembership = userRoleMemberships.get(labLoaderRole);
+
+		    		if (labLoaderRole.isStudyScoped())
+		    		{
+		    			HL7v3CtLabUnMarshaller unMarshaller = new HL7v3CtLabUnMarshaller();
+		    			String studyId = unMarshaller.getStudyId(xml);
+		    			if (studyId == null)
+		    			{
+		    				throw new SuiteAuthorizationAccessException("Role %s is study scoped - study identifier is null", labLoaderRole.getDisplayName());
+		    			}
+		    			
+		    			// if the user has permission to access specific studies (not all studies), then verify the study
+		    			if (!userRoleMembership.isAllStudies() && !userRoleMembership.getStudyIdentifiers().contains(studyId))
+		    		    {
+		    				throw new SuiteAuthorizationAccessException("Username %s is not authorized for study %s", username, studyId);
+		    		    }
+		    		}
+
+		    		if (labLoaderRole.isSiteScoped())
+		    		{
+		    			HL7v3CtLabUnMarshaller unMarshaller = new HL7v3CtLabUnMarshaller();
+		    			String siteNciInstituteCode = unMarshaller.getSiteNciInstituteCode(xml);
+		    			if (siteNciInstituteCode == null)
+		    			{
+		    				throw new SuiteAuthorizationAccessException("Role %s is site scoped - site NCI institute code is null", labLoaderRole.getDisplayName());
+		    			}
+		    			
+		    			// if the user has permission to access specific sites (not all sites), then verify the sites
+		    			if (!userRoleMembership.isAllSites() && !userRoleMembership.getSiteIdentifiers().contains(siteNciInstituteCode))
+		    		    {
+		    				throw new SuiteAuthorizationAccessException("Username %s is not authorized for site %s", username, siteNciInstituteCode);
+		    		    }
+		    		}
+		    	}
+		    }
+		    else
+		    {
+		    	throw new SuiteAuthorizationAccessException("Username %s is not authorized for role %s", username, labLoaderRole.getDisplayName());
+		    }
 		}
 		catch (SuiteAuthorizationAccessException e)
 		{
