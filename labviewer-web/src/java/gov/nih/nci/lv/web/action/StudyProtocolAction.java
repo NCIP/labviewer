5e822530e0f8583288edb67cf050077357f48e0c
Naveen Amiruddin <naveen.amiruddin@gmail.com>
2011-01-28 22:33:02 +0000

    jira 141
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ctodslabviewer/trunk/Code@2403 a393f10d-60de-4571-9f1a-620cac27c168

--- labviewer-web/src/java/gov/nih/nci/lv/web/action/StudyProtocolAction.java
+++ labviewer-web/src/java/gov/nih/nci/lv/web/action/StudyProtocolAction.java
@@ -79,7 +79,6 @@
 
 package gov.nih.nci.lv.web.action;
 
-import gov.nih.nci.cabig.ctms.suite.authorization.SuiteAuthorizationAccessException;
 import gov.nih.nci.lv.dao.StudySearchDAO;
 import gov.nih.nci.lv.dto.IntegrationHubDto;
 import gov.nih.nci.lv.dto.StudySearchDto;
