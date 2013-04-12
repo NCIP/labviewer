ccbefc85ee66c3e3e2d00f7797f76e3049473196
ksambhuni <kumar.sambhuni@semanticbits.com>
2013-04-12 14:31:20 -0400

    add copyright header to JAVA, JSP, XML and Properties files

--- ctomlabviewer/sdkclient/TestClient.java
+++ ctomlabviewer/sdkclient/TestClient.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright ScenPro, Inc and SemanticBits, LLC
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/labviewer/LICENSE.txt for details.
+ */
 import gov.nih.nci.common.util.HQLCriteria;
 import gov.nih.nci.labhub.domain.ConceptDescriptorDataType;
 import gov.nih.nci.labhub.domain.LabResult;
