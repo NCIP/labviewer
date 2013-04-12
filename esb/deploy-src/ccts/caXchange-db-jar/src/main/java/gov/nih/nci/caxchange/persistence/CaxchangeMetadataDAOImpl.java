4e707fbd4eabdfea681fb9676d0fa355597bcedf
ksambhuni <kumar.sambhuni@semanticbits.com>
2013-04-12 15:31:11 -0400

    add copyright header to JAVA, JSP, XML and Properties files

--- esb/deploy-src/ccts/caXchange-db-jar/src/main/java/gov/nih/nci/caxchange/persistence/CaxchangeMetadataDAOImpl.java
+++ esb/deploy-src/ccts/caXchange-db-jar/src/main/java/gov/nih/nci/caxchange/persistence/CaxchangeMetadataDAOImpl.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright ScenPro, Inc and SemanticBits, LLC
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/labviewer/LICENSE.txt for details.
+ */
 package gov.nih.nci.caxchange.persistence;
 
 import gov.nih.nci.caxchange.jdbc.CaxchangeMessage;
--- esb/deploy-src/ccts/caXchange-servicemix-eip-se/src/main/java/gov/nih/nci/caxchange/patterns/CaxchangeItineraryBasedRecipientList.java
+++ esb/deploy-src/ccts/caXchange-servicemix-eip-se/src/main/java/gov/nih/nci/caxchange/patterns/CaxchangeItineraryBasedRecipientList.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright ScenPro, Inc and SemanticBits, LLC
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/labviewer/LICENSE.txt for details.
+ */
 package gov.nih.nci.caxchange.patterns;
 
 import gov.nih.nci.caXchange.CaxchangeConstants;
